7b9c3ed63dddf03642163096146e52b322b03cfb
Pavel Klyuev <71312774+PavelKlyuev@users.noreply.github.com>
2023-04-05 11:21:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,5 @@ import numpy as np
 chat_id = 240671999 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    days = 18
+    return x.mean() / days  # Ваш ответ
